565b3ba3514aee7f77d013ae4120c2df0bcd9ca4
mpaez <paezmorayviviana@hotmail.com>
2023-07-11 22:39:32 -0300

    Proyecto JAVA comision 23069 de Codo a Codo

---
+++ src/main/java/ar/com/codoacodo/controllers/EliminarController.java
@@ -0,0 +1,40 @@
+package ar.com.codoacodo.controllers;
+
+import java.io.IOException;
+
+import ar.com.codoacodo.dao.DAO;
+import ar.com.codoacodo.dao.impl.MysqlDaoImpl;
+//import ar.com.codoacodo23069.Producto;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/EliminarController")
+public class EliminarController extends HttpServlet{
+ 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        
+        String id = req.getParameter("id");
+        
+        DAO dao = new MysqlDaoImpl();
+        
+        try {
+            dao.delete(Long.parseLong(id));
+
+            //redirect al /ListadoController
+            req.setAttribute("eliminadook", "Se ha eliminado el producto id:" + id);
+
+            getServletContext().getRequestDispatcher("/ListadoProductosController").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            req.setAttribute("eliminadofail", "Error eliminando el producto id:" + id);
+
+            getServletContext().getRequestDispatcher("/ListadoProductosController").forward(req, resp);
+        }
+    }
+}
